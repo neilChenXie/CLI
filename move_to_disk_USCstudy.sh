795c729816782afee6a0da673c182c267072e89a
Chen Xie <xcmickey@gmail.com>
2014-04-18 14:22:09 -0700

    V1.0 DFN V1.0 MTDU

---
+++ move_to_disk_USCstudy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#program:
+#	cp to USCstudy
+#History:
+#	No
+#04/18/2014		Chen 	v1.0
+ls
+
+read -p "filename: " file_name
+read -p "which course?: " course
+ 
+ls /media/34B4DE90B4DE53C8/USCstudy/$course
+
+read -p "which folder?: " category
+
+mv /home/neil/Downloads/$file_name /media/34B4DE90B4DE53C8/USCstudy/$course/$category
+
+ls /media/34B4DE90B4DE53C8/USCstudy/$course/$category
